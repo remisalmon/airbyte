504c19cfa87dcb0c2ecfcd12744fffcfcfa57522
Oleksandr Tsukanov <alexander.tsukanovvv@gmail.com>
2021-12-03 10:37:43 +0200

    airbyte-8336: Added comment.

--- airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/constants/GlobalDataSizeConstants.java
+++ airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/constants/GlobalDataSizeConstants.java
@@ -6,5 +6,7 @@ import io.aesy.datasize.DataSize;
 public interface GlobalDataSizeConstants {
   // 256 MB to BYTES as comparison will be done in BYTES
   int MAX_BATCH_SIZE_BYTES = DataSize.of(256L, IEC.MEBIBYTE).toUnit(IEC.BYTE).getValue().intValue();
+  // This constant determines the max size of uploaded chunk of file(e.g. 15 GB / 256 megabytes ≈ 58 MB per chunk of file)
+  // see StagingFilenameGenerator.java:28
   long MAX_BYTE_PARTS_PER_FILE = DataSize.of(15L, IEC.GIBIBYTE).toUnit(IEC.BYTE).getValue().longValue();
 }
